ecc34558b08cf23a272b0aeca2025a335abdd987
“rekhanthms <rekhanthms23@gmail.com>
2024-09-05 10:33:57 +0530

    dsa

--- Solution.java
+++ Solution.java
@@ -69,6 +69,20 @@ public class Solution{
     //     }
     // }
     //#endregion
+    
+    //#inverted half pyramid sapces first
+    public static void main(String arg[]){
+        int n=4;
+        for(int i=1; i<=n;i++){
+            for(int j=1;j<=n-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 
     
 
