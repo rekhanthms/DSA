58d8dec60fb84480d5aa1299c49bb18fb9d65196
“rekhanthms <rekhanthms23@gmail.com>
2024-09-10 22:24:22 +0530

    butterfly

--- Patterns/Adavance_pattern.java
+++ Patterns/Adavance_pattern.java
@@ -1,6 +1,8 @@
-package Patterns.java;
+package Patterns;
 
 public class Adavance_pattern {
+    
+    //# BUTTERFLY
     public static void main(String args[]){
         int n=5;
         for(int i=1;i<=n;i++){
@@ -28,5 +30,5 @@ public class Adavance_pattern {
             System.err.println();
         }
     }
-    
+    //#endregion
 }
