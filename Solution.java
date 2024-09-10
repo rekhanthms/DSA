6fc339a2a786e13d1a3a59f7360965f051f9805f
“rekhanthms <rekhanthms23@gmail.com>
2024-09-10 20:42:18 +0530

    dsa

--- Solution.java
+++ Solution.java
@@ -1,89 +1,132 @@
 import java.util.*;
 
-
-public class Solution{
-    //#solid rectangle
+public class Solution {
+    // #solid rectangle
     // public static void main(String arg[]){
-    //     int n=4,m=6;
-    //     for(int i=0;i<n;i++){
-    //         for(int j=0;j<m;j++){
-    //             System.out.print("*");
-    //         }
-    //         System.err.println();
-    //     }
+    // int n=4,m=6;
+    // for(int i=0;i<n;i++){
+    // for(int j=0;j<m;j++){
+    // System.out.print("*");
     // }
-    //#endregion
+    // System.err.println();
+    // }
+    // }
+    // #endregion
 
-     //#Hollow Rectangle 
+    // #Hollow Rectangle
     // public static void main(String arg[]){
-    //     int n=4,m=4;
-    //     for(int i=0;i<n;i++){
-    //         for(int j=0;j<m;j++){
-    //             if(i==0|| j==0|| i==n-1|| j==m-1){
-    //                 System.out.print("*");
-    //             }
-    //             else{
-    //                 System.out.print(" ");
-    //             }
-    //         }
-    //         System.out.println();
-    //     }
+    // int n=4,m=4;
+    // for(int i=0;i<n;i++){
+    // for(int j=0;j<m;j++){
+    // if(i==0|| j==0|| i==n-1|| j==m-1){
+    // System.out.print("*");
     // }
-    //#endregion
+    // else{
+    // System.out.print(" ");
+    // }
+    // }
+    // System.out.println();
+    // }
+    // }
+    // #endregion
 
-    //#Half Pyramid
+    // #Half Pyramid
     // public static void main(String arg[]){
-    //     int n=4;
-    //     for(int i=0; i<=n;i++){
-    //         for(int j=0;j<i;j++){
-    //             System.out.print("*");
-    //         }
-    //         System.out.println();
-    //     }
+    // int n=4;
+    // for(int i=0; i<=n;i++){
+    // for(int j=0;j<i;j++){
+    // System.out.print("*");
     // }
-    //#endregion
+    // System.out.println();
+    // }
+    // }
+    // #endregion
 
-    //#Inverted half pyramid 
+    // #Inverted half pyramid
     // public static void main(String arg[]){
     // int n=4;
     // for( int i=n;i>=0;i--){
-    //     for(int j=1;j<=i;j++){
-    //         System.out.print("*");
-    //     }
-    //     System.out.println(" ");
+    // for(int j=1;j<=i;j++){
+    // System.out.print("*");
+    // }
+    // System.out.println(" ");
     // }
     // }
-    //#endregion
+    // #endregion
 
-    //#Inverted half Pyramid
+    // #Inverted half Pyramid
     // public static void main(String arg[]){
-    //     int n=4;
-    //     for(int i=1; i<=n;i++){
-    //         for(int j=1;j<=n-i;j++){
-    //             System.out.print(" ");
+    // int n=4;
+    // for(int i=1; i<=n;i++){
+    // for(int j=1;j<=n-i;j++){
+    // System.out.print(" ");
+    // }
+    // for(int j=1;j<=i;j++){
+    // System.out.print("*");
+    // }
+    // System.out.println();
+    // }
+    // }
+    // #endregion
+
+    // # Numnber pyramid
+    // public static void main(String args[]){
+    // int n=5;
+    // for(int i=1;i<=n;i++){
+    // for(int j=1;j<=i;j++){
+    // System.out.print(j+" ");
+    // }
+    // System.out.println();
+    // }
+    // }
+    // #endregion
+
+    // # Reversed pyramid numbers
+    // public static void main(String arg[]){
+    // int n=5;
+    // for(int i=1;i<=n;i++){
+    // for(int j=1;j<=n-i+1;j++){
+    // System.out.print(j);
+    // }
+    // System.out.println();
+    // }
+    // }
+    // #region
+
+    //# Flayd's triangle
+    // public static void main(String arg[]) {
+    //     int n = 5;
+    //     int num = 1;
+    //     for (int i = 1; i <= n; i++) {
+    //         for (int j = 1; j <= i; j++) {
+    //             System.out.print(num + " ");
+    //             num++;
     //         }
+    //         System.out.println();
+    //     }
+    // }
+    //#region
+
+    //# 00 11 triangle
+    // public static void main(String args[]){
+    //     int n=5;
+    //     int sum=0;
+    //     for(int i=1;i<=n;i++){
     //         for(int j=1;j<=i;j++){
-    //             System.out.print("*");
+    //             sum=i+j;
+    //             if(sum%2==0){
+    //                 System.out.print(1);
+    //             }
+    //             else{
+    //                 System.out.print(0);
+    //             }
     //         }
     //         System.out.println();
     //     }
     // }
     //#endregion
-    
-    //#inverted half pyramid sapces first
-    public static void main(String arg[]){
-        int n=4;
-        for(int i=1; i<=n;i++){
-            for(int j=1;j<=n-1;j++){
-                System.out.print(" ");
-            }
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
 
     
+
 
 }
